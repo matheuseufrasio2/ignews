9d85dbcc6099f9d3738eb400bade50d6a30ae746
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2021-04-01 01:57:09 -0300

    app finalizado | add previews e posts de acordo com o login do usuario.

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,18 +1,31 @@
 import { useSession, signIn } from 'next-auth/client';
+import { Session } from 'next-auth';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
+import { useRouter } from 'next/dist/client/router';
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
+interface SessionProps extends Session {
+  activeSubscription?: unknown;
+}
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
+  const router = useRouter();
+  const sessionEdited: SessionProps = session;
 
   async function handleSubscribe() {
     if (!session) {
       signIn('github');
+      return;
+    }
+
+    if (sessionEdited.activeSubscription) {
+      router.push("/posts");
       return;
     }
 
